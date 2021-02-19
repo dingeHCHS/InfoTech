70fb25967561affba18d5e5c6a670322e90d799a
dingeHCHS <69469881+dingeHCHS@users.noreply.github.com>
2021-02-19 12:12:21 -0600

    ahh

---
+++ data.js
@@ -0,0 +1,61 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-infotech",
+      "name": "InfoTech",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1520,
+      "initialViewParameters": {
+        "yaw": -3.1285388707109067,
+        "pitch": -0.0806615934687045,
+        "fov": 1.2933824216075565
+      },
+      "linkHotspots": [],
+      "infoHotspots": [
+        {
+          "yaw": 3.1237304631212757,
+          "pitch": -0.03929584977448464,
+          "title": "Intro",
+          "text": "Text"
+        },
+        {
+          "yaw": -2.543516484451766,
+          "pitch": -0.010965753882150153,
+          "title": "IT1",
+          "text": "Text"
+        },
+        {
+          "yaw": 2.6134633715437676,
+          "pitch": -0.011273488800615183,
+          "title": "IT2",
+          "text": "Text"
+        }
+      ]
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": false,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
