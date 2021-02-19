cb7f611cfee172a8d2645a7469ca207224c1b9ae
dingeHCHS <69469881+dingeHCHS@users.noreply.github.com>
2021-02-19 12:15:40 -0600

    Update data.js

--- data.js
+++ data.js
@@ -34,19 +34,19 @@ var APP_DATA = {
           "yaw": 3.1237304631212757,
           "pitch": -0.03929584977448464,
           "title": "Intro",
-          "text": "Text"
+          "text": '<iframe src="https://drive.google.com/file/d/1JVEhqbW7BA7v7WVpzf9AkPNrEryBIDmn/preview" width="640" height="480"></iframe>'
         },
         {
           "yaw": -2.543516484451766,
           "pitch": -0.010965753882150153,
           "title": "IT1",
-          "text": "Text"
+          "text": '<iframe src="https://drive.google.com/file/d/1JYjrDBr2Ezby81kx7uFu8QCP9hjWKXZg/preview" width="640" height="480"></iframe>'
         },
         {
           "yaw": 2.6134633715437676,
           "pitch": -0.011273488800615183,
           "title": "IT2",
-          "text": "Text"
+          "text": '<iframe src="https://drive.google.com/file/d/1Jgq4bB4QY6BzjYge9QYupCVLVN_E6FiA/preview" width="640" height="480"></iframe>'
         }
       ]
     }
